7e1f0a38020e78df126adede4b06c3c2a62b3905
Nitin Baghel <57316054+Nitin408@users.noreply.github.com>
2021-01-01 18:15:09 +0530

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(arr):
+    for i in range(len(arr) - 1):
+        swapped = False
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            return
+ 
+ 
+arr = [int(x) for x in input(‘Enter numbers: ’).split()]
+bubble_sort(arr)
+print('Sorted list: ', end='')
+print(arr)
